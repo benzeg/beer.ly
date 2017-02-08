d9a1a0864d1e54a8310e04177461db887fec385e
Benze Gong <Benze Gong>
2017-02-08 11:04:40 -0800

    updated signup/login cb to return user information

--- db/controllers/authController.js
+++ db/controllers/authController.js
@@ -2,29 +2,34 @@ var db = require('../index.js');
 var bcrypt = require('bcrypt-nodejs');
 
 var userSignup = function(userInput, cb) {
-  var username = userInput['username'];
-  var password = userInput['password'];
-  var phonenumber = userInput['phonenumber'];
-  var location = userInput['location'];
+  var username = userInput.username;
+  var password = userInput.password;
+  var phonenumber = userInput.phonenumber;
+  var location = userInput.location;
   db.Customers.findOrCreate({where: {username: username},
   	defaults: {password: password, 
   			   phonenumber: phonenumber,
   			   location: location}})
   .spread(function(user, created) {
   	if (created === false) {
-  	//user already exisits
+  	//user already exists
   	  var error = "Username already in use";
   	  cb(error);
   	} else {
   	//new user created
-  	  cb(null, user);
+      //create a new User object without password field to be passed to server-side
+      var newUser = {};
+      newUser.username = user.username;
+      newUser.phonenumber = user.phonenumber;
+      newUser.location = user.location;
+  	  cb(null, newUser);
   	}
   });
 };
 
 var userLogin = function(userInput, cb) {
-  var username = userInput['username'];
-  var password = userInput['password'];
+  var username = userInput.username;
+  var password = userInput.password;
 
   db.Customers.findOne({where: {username: username}})
   .then(function(user) {
@@ -33,7 +38,11 @@ var userLogin = function(userInput, cb) {
  	  	var error = "Username/Password do not match";
  	  	cb(error);
  	  } else {
- 	  	cb(null, res);
+      var newUser = {};
+      newUser.username = user.username;
+      newUser.phonenumber = user.phonenumber;
+      newUser.location = user.location;
+ 	  	cb(null, newUser);
  	  }
   	})
   });
