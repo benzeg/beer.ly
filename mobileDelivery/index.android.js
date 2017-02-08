03e10135f7c0d236d9c104e63379452b76164547
sscchan <stephen.s.c.chan@gmail.com>
2017-02-08 11:15:49 -0800

    add barebone update status functionality to app

--- mobileDelivery/index.android.js
+++ mobileDelivery/index.android.js
@@ -4,6 +4,11 @@
  * @flow
  */
 
+
+// Uses React Native Materials UI
+// https://www.npmjs.com/package/react-native-material-ui
+
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,42 +17,101 @@ import {
   View
 } from 'react-native';
 
+import {
+  ThemeProvider,
+  COLOR,
+  ListItem,
+  Toolbar
+} from 'react-native-material-ui';
+
+import Container from './app/components/Container.js';
+
+// you can set your style right here, it'll be propagated to application
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.green500,
+  },
+  toolbar: {
+    container: {
+      height: 50,
+    },
+  },
+};
+
+//
 export default class mobileDelivery extends Component {
+  componentDidMount() {
+    
+  }
+
+  updateStatus(newStatus) {
+    const USERNAME = 'boba';
+    const PASSWORD = 'hunter2';
+
+    const TEST_SERVER_ADDRESS = 'http://requestb.in/15ooxvh1';
+    fetch(TEST_SERVER_ADDRESS, {
+      method: 'POST',
+      headers: {
+        'Accept': 'applications/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: USERNAME,
+        password: PASSWORD,
+        status: newStatus
+      })
+    })
+    .then(response => console.log('Response:', response))
+    .catch(error=> console.log('Error:', error));
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <ThemeProvider uiTheme={uiTheme}>
+        <Container>
+          <Toolbar
+            leftElement="menu"
+            centerElement="Beer.ly Delivered"
+          />
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: 'Delivery Job Accepted'
+            }}
+            onPress={()=>this.updateStatus('Delivery Job Accepted')}
+          />
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: 'Enroute to Supply Warehouse'
+            }}
+            onPress={()=>this.updateStatus('Enroute to Supply Warehouse')}
+          />
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: 'Loading Goods'
+            }}
+            onPress={()=>this.updateStatus('Loading Goods')}
+          />
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: 'Enroute to Customer'
+            }}
+            onPress={()=>this.updateStatus('Enroute to Customerd')}
+          />
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: 'Delivered to Customer'
+            }}
+            onPress={()=>this.updateStatus('Delivered to Customer')}
+          />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('mobileDelivery', () => mobileDelivery);
