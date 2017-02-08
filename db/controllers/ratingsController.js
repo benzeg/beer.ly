534e3650b03a746570cb6fd1a7a109c616b5d1e3
Benze Gong <Benze Gong>
2017-02-08 11:32:00 -0800

    added sequelize to package.json

--- db/controllers/ratingsController.js
+++ db/controllers/ratingsController.js
@@ -1,8 +1,19 @@
-// var db = require('../index.js');
+var db = require('../index.js');
 
-// var saveRating = function(userRating, cb) {
-//   db.Customers.findOne({where: {username: username}})
-//   .then(function(user) {
-  	
-//   });
-// }
+var saveRating = function(userRating, cb) {
+  var username = userRating.username;
+  var rating = userRating.rating;
+  var product = userRating.product;
+  //get customer ID from Customers database
+  // db.Customers.findOne({where: {username: username}})
+  // .then(function(user) {
+  // 	var customerId = user.id;
+  // 	//save rating to ProductRatings database
+  // 	db.ProductRatings.findOrCreate({where: {product: product, rating: rating, }})
+
+
+
+  // }).catch(function(err) {
+  // 	cb(err);
+  // });
+}
