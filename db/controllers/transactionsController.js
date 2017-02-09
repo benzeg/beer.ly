436b9830d4fccef3c6f2d25f31106d866416aa4d
Benze Gong <Benze Gong>
2017-02-09 10:49:50 -0800

    updated getRatings db controller

--- db/controllers/transactionsController.js
+++ db/controllers/transactionsController.js
@@ -1,5 +1,9 @@
 var db = require('../index.js');
 
 exports.saveTransaction = function(transaction, cb) {
-
+  db.Transactions.create(transaction).then(function() {
+  	cb(null, );
+  }).catch(function(err) {
+  	cb(err);
+  })
 };
