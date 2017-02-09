e8a61ef66bd8c7d52672b161997431c84fca789a
sscchan <stephen.s.c.chan@gmail.com>
2017-02-08 19:32:32 -0800

    implement google map view

--- mobileDelivery/app/App.js
+++ mobileDelivery/app/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import { SERVER_ADDRESS } from './config/ServerConfig.js';
 import Container from './components/Container.js';
@@ -9,18 +10,29 @@ import { Toolbar } from 'react-native-material-ui';
 import { ActionButton } from 'react-native-material-ui';
 import { ListItem } from 'react-native-material-ui';
 
+import MapView from 'react-native-maps';
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      location: {},
+      location: {
+        latitude: 37.78825,
+        longitude: -122.4324
+      },
       deliveryStatus: 'No Job Active',
       isSelectingUpdatedJobStatus: false
     };
   }
 
   handleStatusUpdate = (newStatus) => {
+    this.setState({
+      deliveryStatus: newStatus !== null ? newStatus : this.state.deliveryStatus,
+      isSelectingUpdatedJobStatus: false
+    });
+
     const USERNAME = 'boba';
     const PASSWORD = 'hunter2';
     const JOBID = 'job123';
@@ -53,27 +65,56 @@ class App extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <Container>
         <Toolbar
           leftElement="menu"
           centerElement="Beer.ly Delivered"
         />
 
-
-        {this.state.isSelectingUpdatedJobStatus && 
+        {this.state.isSelectingUpdatedJobStatus &&
           <DeliveryStatusUpdateOptionsList onStatusUpdate={this.handleStatusUpdate}/>
         }
 
-        <LocationTracker onLocationChange={this.handleLocationChange}/>
+        <View>
+          <LocationTracker onLocationChange={this.handleLocationChange}/>
 
-        <ListItem
-          divider
-          centerElement={{
-            primaryText: this.state.deliveryStatus
-          }}
-          onPress={()=> this.setState({isSelectingUpdatedJobStatus: !this.state.isSelectingUpdatedJobStatus})}
-        />
+          <MapView
+            initialRegion={{
+              latitude: this.state.location.latitude,
+              longitude: this.state.location.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+            region={{
+              latitude: this.state.location.latitude,
+              longitude: this.state.location.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+            style={{
+              width: null,
+              height: 400
+            }}
+          >
+
+            <MapView.Marker
+              coordinate={{latitude: this.state.location.latitude, longitude: this.state.location.longitude}}
+              title={'title'}
+              description={'description'}
+            />
+          </MapView>
+
+          <ListItem
+            divider
+            centerElement={{
+              primaryText: this.state.deliveryStatus
+            }}
+            onPress={()=> this.setState({isSelectingUpdatedJobStatus: !this.state.isSelectingUpdatedJobStatus})}
+          />
+        </View>
+
+
       </Container>
     );
   }
