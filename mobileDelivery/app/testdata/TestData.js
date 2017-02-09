9fd7a2385e4c1813d40f81094ddfb9e3087e6920
sscchan <stephen.s.c.chan@gmail.com>
2017-02-09 15:32:36 -0800

    move test data to dedicated test data module

---
+++ mobileDelivery/app/testdata/TestData.js
@@ -0,0 +1,86 @@
+export const PendingJobTestData = [
+  {
+    id: 'testID123',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID124',
+    supplyAddresses: ['563 2nd St, San Francisco', '1150 Howard St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'mary',
+    customerPhoneNumber: '555-555-7777'
+  },
+  {
+    id: 'testID125',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID126',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID127',
+    supplyAddresses: ['563 2nd St, San Francisco', '1150 Howard St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'mary',
+    customerPhoneNumber: '555-555-7777'
+  },
+  {
+    id: 'testID128',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID129',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID1211',
+    supplyAddresses: ['563 2nd St, San Francisco', '1150 Howard St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'mary',
+    customerPhoneNumber: '555-555-7777'
+  },
+  {
+    id: 'testID1212',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID12113',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+  {
+    id: 'testID1214',
+    supplyAddresses: ['563 2nd St, San Francisco', '1150 Howard St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'mary',
+    customerPhoneNumber: '555-555-7777'
+  },
+  {
+    id: 'testID1213',
+    supplyAddresses: ['1705 Mariposa St, San Francisco', '563 2nd St, San Francisco'],
+    deliveryAddress: '555 Commercial Street, San Francisco',
+    customerName: 'bob',
+    customerPhoneNumber: '555-555-5555'
+  },
+];
