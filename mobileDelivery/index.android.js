26f99ba565044bcb3d7d0d0385c45d05f4e015fc
sscchan <stephen.s.c.chan@gmail.com>
2017-02-08 19:32:52 -0800

    moved code to App component

--- mobileDelivery/index.android.js
+++ mobileDelivery/index.android.js
@@ -10,23 +10,16 @@
 
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import {
   ThemeProvider,
   COLOR,
-  ListItem,
-  Toolbar
 } from 'react-native-material-ui';
 
-import Container from './app/components/Container.js';
+import App from './app/App.js';
 
-// you can set your style right here, it'll be propagated to application
+// default UI Theme
 const uiTheme = {
   palette: {
     primaryColor: COLOR.green500,
@@ -40,78 +33,18 @@ const uiTheme = {
 
 //
 export default class mobileDelivery extends Component {
-  componentDidMount() {
-    
-  }
-
-  updateStatus(newStatus) {
-    const USERNAME = 'boba';
-    const PASSWORD = 'hunter2';
-
-    const TEST_SERVER_ADDRESS = 'http://requestb.in/15ooxvh1';
-    fetch(TEST_SERVER_ADDRESS, {
-      method: 'POST',
-      headers: {
-        'Accept': 'applications/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: USERNAME,
-        password: PASSWORD,
-        status: newStatus
-      })
-    })
-    .then(response => console.log('Response:', response))
-    .catch(error=> console.log('Error:', error));
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <ThemeProvider uiTheme={uiTheme}>
-        <Container>
-          <Toolbar
-            leftElement="menu"
-            centerElement="Beer.ly Delivered"
-          />
-          <ListItem
-            divider
-            centerElement={{
-              primaryText: 'Delivery Job Accepted'
-            }}
-            onPress={()=>this.updateStatus('Delivery Job Accepted')}
-          />
-          <ListItem
-            divider
-            centerElement={{
-              primaryText: 'Enroute to Supply Warehouse'
-            }}
-            onPress={()=>this.updateStatus('Enroute to Supply Warehouse')}
-          />
-          <ListItem
-            divider
-            centerElement={{
-              primaryText: 'Loading Goods'
-            }}
-            onPress={()=>this.updateStatus('Loading Goods')}
-          />
-          <ListItem
-            divider
-            centerElement={{
-              primaryText: 'Enroute to Customer'
-            }}
-            onPress={()=>this.updateStatus('Enroute to Customerd')}
-          />
-          <ListItem
-            divider
-            centerElement={{
-              primaryText: 'Delivered to Customer'
-            }}
-            onPress={()=>this.updateStatus('Delivered to Customer')}
-          />
-        </Container>
+          <App />
       </ThemeProvider>
     );
   }
 }
+
 
 AppRegistry.registerComponent('mobileDelivery', () => mobileDelivery);
