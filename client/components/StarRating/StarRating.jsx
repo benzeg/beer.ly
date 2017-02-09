4b6cf3d9c9b0c12bf2c0c2a6807382aa82ceb3a9
James Falkoff <james.falkoff@gmail.com>
2017-02-08 16:20:52 -0800

    add StarRating component to BeerItem

---
+++ client/components/StarRating/StarRating.jsx
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import { Rating } from 'material-ui-rating';
+
+class StarRating extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+      readOnly: false,
+      max: 4
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({
+      value: value
+    });
+
+    this.props.onClick(value);
+  }
+
+  render() {
+    return (
+      <Rating value={this.state.value} max={this.state.max} readOnly={this.state.readOnly} onChange={this.handleChange} />
+    );
+  }
+}
+
+StarRating.propTypes = {
+  onClick: PropTypes.func
+};
+
+export default StarRating;
