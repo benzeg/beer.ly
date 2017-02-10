da89d9ba07233fb38b283bdfbb56f858af5b2be5
sscchan <stephen.s.c.chan@gmail.com>
2017-02-09 20:13:51 -0800

    implemented driving route display on dashboard

--- mobileDelivery/app/layouts/ActiveJobDashBoardLayout.js
+++ mobileDelivery/app/layouts/ActiveJobDashBoardLayout.js
@@ -1,19 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 
-import { View, Text } from 'react-native';
-import { ScrollView } from 'react-native';
 import { Dimensions } from 'react-native';
-
 import { ListItem } from 'react-native-material-ui';
-
 import MapView from 'react-native-maps';
 
-import Styles from '../Styles.js';
 import Container from '../components/Container.js';
+
+import { SERVER_ADDRESS, USERNAME, PASSWORD } from '../config/ServerConfig.js';
+import PolylineDecoder from '../lib/PolylineDecoder.js';
 
 class ActiveJobDashBoardLayout extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      navigationRoute: []
+    };
+
+    this.oldLatitude = 0;
+    this.oldLongitude = 0;
+  }
+
+  componentWillMount() {
+    this.drivingRouteRefreshID = setInterval(()=> this.updateDrivingRoute(), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.drivingRouteRefreshID);
+  }
+
+  updateDrivingRoute() {
+    // Only update if the latitude and longitiude has changed, this minimised unneeded calls to
+    // Google Directino API
+    if (this.oldLongitude === this.props.location.longitude &&
+        this.oldLatitude === this.props.location.latitude) {
+      return;
+    }
+
+    // Construct the Google Direction API request with query string
+    const directionRequestURL = 'https://maps.googleapis.com/maps/api/directions/json?' +
+       'origin=' + this.props.location.latitude.toFixed(4) + ',' + this.props.location.longitude.toFixed(4) +
+       '&destination=' + this.props.job.deliveryAddress +
+       '&waypoints=optimize:true|' + this.props.job.supplyAddresses.join('|') +
+       '&key=AIzaSyBvksqXve801sMkha2K6zdd8oGqXd_fhjk';
+
+    // Starting the call
+    axios({
+      method: 'post',
+      url: directionRequestURL,
+      data: {
+      }
+    }).then((response) => {
+      this.setState({
+        navigationRoute: PolylineDecoder(response.data.routes[0].overview_polyline.points)
+      });
+
+      // Save the location as the 'old' location value for future comparison
+      this.oldLatitude = this.props.location.latitude;
+      this.oldLongitude = this.props.location.longitude;
+
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
 
   render() {
@@ -42,6 +91,14 @@ class ActiveJobDashBoardLayout extends React.Component {
 
           <MapView.Marker coordinate={{latitude: this.props.location.latitude, longitude: this.props.location.longitude}} />
 
+          {this.state.navigationRoute.length > 1 &&
+            <MapView.Polyline
+            coordinates={this.state.navigationRoute}
+            strokeWidth={5}
+            strokeColor={'#f00'}
+            />
+          }
+
         </MapView>
 
         <ListItem
@@ -56,6 +113,13 @@ class ActiveJobDashBoardLayout extends React.Component {
     );
   }
 }
+
+ActiveJobDashBoardLayout.propTypes = {
+  job: React.PropTypes.object,
+  location: React.PropTypes.object,
+  onJobStatusUpdateClick: React.PropTypes.func,
+  deliveryStatus: React.PropTypes.string
+};
 
 ActiveJobDashBoardLayout.defaultProps = {
   location: {
