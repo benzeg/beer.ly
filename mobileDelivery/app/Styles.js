70dbc320408c7253c69cf00d9c1b69d0e8f7a2c6
sscchan <stephen.s.c.chan@gmail.com>
2017-02-09 16:19:47 -0800

    add styles for pending delivery job Card's text

--- mobileDelivery/app/Styles.js
+++ mobileDelivery/app/Styles.js
@@ -3,20 +3,22 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    justifyContent: 'center'
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  jobCard: {
+    padding: 10
+  },
+  jobTitle: {
+    fontWeight: 'bold'
+  },
+  jobText: {
+
+  }
 });
 
 export default styles;
