6a154aa2f5d9bfdfd73cf2a21364802cc8bd34d9
Benze Gong <Benze Gong>
2017-02-10 13:35:56 -0800

    completed recommendations function

--- server/middleware/recommendations.js
+++ server/middleware/recommendations.js
@@ -9,13 +9,13 @@ exports.getRecommendedStyleIds = function(customer, cb) {
   	  var ratingsObj = {};
   	  ratings.forEach(function(rating) {
   	  	var styleId = rating.productStyleId;
-  	  	if (ratingsObj[styleId] === null) {
+  	  	if (!ratingsObj[styleId]) {
   	  		ratingsObj[styleId] = {average:0, setNum:0};
   	  	};
-  	  	var sum = ratingsObj[styleId].average * setNum;
+  	  	var sum = ratingsObj[styleId].average * ratingsObj[styleId].setNum;
   	  	sum += rating.rating;
-  	  	setNum += 1;
-  	  	ratingsObj[styleId].average = sum / setNum;
+  	  	ratingsObj[styleId].setNum += 1;
+  	  	ratingsObj[styleId].average = sum / ratingsObj[styleId].setNum;
   	  });
 
   	  var recommendedStyleIds = [];
@@ -31,18 +31,29 @@ exports.getRecommendedStyleIds = function(customer, cb) {
 };
 
 exports.getRecommendedProducts = function(styleIdArray, cb) {
+  //initialize empty beer array
   var beerArray = [];
+  var counter = 0;
+  var size = styleIdArray.length;
+  //for each styleId in array of styles,
+    //make api call to BreweryDB to obtain list of beers pertaining to each styleId
   styleIdArray.forEach(function(styleId) {
   	beerController.fetchBeersByStyleId(styleId)
   	.then(function(response) {
-  	  var beerArray = response.data;
-  	  beerArray = exports.randomize(beerArray);
-  	  cb(null, beerArray);
+  	  var newbeerArray = response.data;
+  	  //extract 5 random beers from response
+  	    //concat to beerArray
+  	  newbeerArray = exports.randomize(newbeerArray);
+  	  beerArray = beerArray.concat(newbeerArray);
+  	  counter++;
+  	  if (counter === size) {
+  	  	cb(null, beerArray);
+  	  }
   	}).catch(function(err) {
   	  cb(err);
   	})
-  })
-}
+  });
+};
 
 exports.randomize = function(beerArray) {
   var counter = 0;
